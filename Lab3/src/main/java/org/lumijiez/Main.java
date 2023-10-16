8e4c32df6808e5b137e940f55b4c8510849ae196
Daniel <danthevip@gmail.com>
2023-10-16 18:01:50 +0300

    Created Lab3 module

--- Lab3/src/main/java/org/lumijiez/Main.java
+++ Lab3/src/main/java/org/lumijiez/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
