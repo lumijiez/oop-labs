94523883fb722cccb7bd60501f4ba86acf2fee37
Daniel <danthevip@gmail.com>
2023-10-27 11:58:06 +0300

    Less buggy regex

--- Lab3/src/main/java/org/lumijiez/base/Document.java
+++ Lab3/src/main/java/org/lumijiez/base/Document.java
@@ -87,6 +87,12 @@ public class Document extends File implements IDocument {
 
     @Override
     public String getInfo() {
-        return "Name: " + getName() + " Size: " + getFilesizeKB() + "Type: " + getFileType().getTypeName();
+        StringBuilder info = new StringBuilder();
+        info.append("Type: ").append(getFileType().getTypeName()).append("<br>");
+        info.append("Extension: ").append(getExtension().toUpperCase()).append("<br>");
+        info.append("File size: ").append(getFilesizeKB()).append(" KB").append("<br>");
+        info.append("Created at: ").append(getCreatedTime()).append("<br>");
+        info.append("Modified at: ").append(getModificationTime()).append("<br>");
+        return info.toString();
     }
 }
